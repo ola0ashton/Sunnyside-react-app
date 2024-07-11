6c6d1d38bfe8f0df3ee1559f033a9ef474ca8035
Pascal954 <agwanirupascal18@gmail.com>
2024-07-11 13:30:52 +0100

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './component/Header.jsx';
+import Main from './component/Main.jsx';
+import Testimonials from './component/Testimonials.jsx';
+import Pictures from './component/pictures.jsx';
+import Footer from './component/Footer.jsx';
+import './App.css'; 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Testimonials />
+      <Pictures />
+      <Footer />
     </div>
   );
 }
